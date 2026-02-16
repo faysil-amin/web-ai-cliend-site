9491c7008c95c3979b5005681df666a71f03c854
faysil-amin <foodbrochandpur@gmail.com>
2026-02-16 09:51:47 +0600

    ADD hook useLink

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Container from "../Container/Container";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="bg-neutral text-neutral-content">
+      <Container>
+        <footer className="mt-8 footer sm:footer-horizontal  p-10">
+          <aside>
+            <h1 className="text-2xl font-bold">Web AI</h1>
+            <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+          </aside>
+          <nav>
+            <h6 className="footer-title">Social</h6>
+            <div className="flex flex-col gap-2 gap-4">
+              <a
+                target="_blank"
+                href="https://github.com/faysil-amin/web-ai-cliend-site"
+                className="flex items-center gap-2"
+              >
+                <span className="text-2xl">
+                  <FaGithub />
+                </span>
+                Clend site git link{" "}
+              </a>
+              <a
+                target="_blank"
+                href="https://github.com/faysil-amin/web-ai-server-site"
+                className="flex items-center gap-2"
+              >
+                <span className="text-2xl">
+                  <FaGithub />
+                </span>
+                server site git link{" "}
+              </a>
+            </div>
+          </nav>
+        </footer>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,20 +2,36 @@ import React, { useEffect, useState } from "react";
 import Container from "../../Component/Container/Container";
 import useAxios from "../Auth/useAxios/useAxios";
 import Card from "../Card/Card";
-
+import Loader from "../Loader/Loader";
 const Home = () => {
+  const [load, setLoad] = useState(true);
   const [data, setData] = useState([]);
-  console.log(data);
   const axios = useAxios();
   useEffect(() => {
-    axios.get("/add-latest-model").then((data) => setData(data.data));
+    axios
+      .get("/add-latest-model")
+      .then((res) => {
+        setData(res.data);
+        setLoad(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoad(false);
+      });
   }, [axios]);
+
   return (
     <Container>
-      <div className="grid grid-cols-3 gap-2 mt-8">
-        {data.map((res) => (
-          <Card res={res}></Card>
-        ))}
+      <div>
+        {load ? (
+          <Loader></Loader>
+        ) : (
+          <div className="grid grid-cols-3 gap-2 mt-8">
+            {data.map((res) => (
+              <Card res={res}></Card>
+            ))}
+          </div>
+        )}
       </div>
     </Container>
   );
